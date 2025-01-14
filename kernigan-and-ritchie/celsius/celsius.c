fad9cff8d1b01138f40336aa255676ca1170dbb3
Yi Fong Au <yifongau@gmail.com>
2025-01-14 21:35:03 +0100

    fahr celsius converter

--- kernigan-and-ritchie/celsius/celsius.c
+++ kernigan-and-ritchie/celsius/celsius.c
@@ -2,7 +2,7 @@
 
 main()
 {
-  int fahr, celsius;
+  float fahr, celsius;
   int lower, upper, step;
 
   lower = 0;
@@ -10,9 +10,18 @@ main()
   step = 20;
 
   fahr = lower;
+  
+  printf("%4s\t%6s\t\t", "FAHR", "CELSIUS");
+  printf("%6s\t%4s\n", "CELSIUS", "FAHR");
+
   while (fahr <= upper) {
-    celsius = 5 * (fahr-32) /9;
-    printf("%d\t%d\n", fahr, celsius);
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%4.0f\t%7.1f\t\t", fahr, celsius);
+    printf("%7.1f\t%4.0f\n", celsius, fahr);
     fahr = fahr + step;
   }
+
+
 }
+
+
